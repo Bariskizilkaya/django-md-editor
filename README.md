1f1967ecb32ec46fe8ca116ec6ba99b8de07e581
Hakan Çelik <hakancelik96@outlook.com>
2018-05-13 21:47:46 +0300

    setup,readme

---
+++ README.md
@@ -0,0 +1,54 @@
+# Django-md-editor
+
+> Django-md-editor package helps integrate [editor.md](https://github.com/pandao/editor.md) with Django.
+
+## Getting started
+
+Install the package:
+
+  `pip install djmd`
+
+
+Add `djmd` to INSTALLED_APPS in `settings.py`.
+
+
+## Usage
+
+1. Usage in models
+
+```python
+from djmd.models import EditorMdField
+
+
+class Page(models.Model):
+    content_editormd1 = EditorMdField()
+```
+
+2. Usage in forms
+
+```python
+from djmd.fields import EditorMdFormField
+
+
+class PageForm(forms.ModelForm):
+    content_editormd1 = EditorMdFormField()
+    content_editormd2 = EditorMdFormField()
+    content_editormd3 = EditorMdFormField()
+
+    class Meta:
+        model = Page
+        fields = '__all__'
+
+
+@admin.register(Page)
+class PageAdmin(admin.ModelAdmin):
+    form = PageForm
+```
+
+3. To show detail page your web site
+- install djmd_tags
+`{% load djmd_tags %}`
+- and use
+```
+blabla|md_detail:"content"|safe
+```
---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+
+setup(
+    name="djmd",
+    version='0.0.1',
+    description='djmd package helps integrate editor.md with Django.',
+    long_description=open('README.md').read(),
+    author='hakancelik96',
+    author_email='hakancelik96@outlook.com',
+    packages=['djmd'],
+    include_package_data=True,
+    install_requires=[],
+    url="https://github.com/coogger/django-md-editor",
+    license='MIT',
+    zip_safe=False,
+    keywords='editor.md,django,editor,text,editor,rich,markdown,md',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+)
